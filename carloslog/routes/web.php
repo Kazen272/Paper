3e05f5dd97d6d436c3c469c114fd366ac2056cc4
Kazen272 <cadudps@gmail.com>
2024-07-01 03:41:41 -0300

    Atualização

--- carloslog/routes/web.php
+++ carloslog/routes/web.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\controllers\HomeController;
-use App\Http\controllers\EntregasController;
+use App\Http\controllers\EventController;
 use App\Http\controllers\LoginController;
 use App\Http\controllers\CadastroController;
 
 Route::get('/', [HomeController::class, 'Home']);
-Route::get('/entregas', [EntregasController::class, 'Entregas']);
+Route::get('/entregas', [EventController::class, 'create']);
 Route::get('/login', [LoginController::class, 'Login']);
 Route::get('/cadastro', [CadastroController::class, 'Cadastro']);
 
