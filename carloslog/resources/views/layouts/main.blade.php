3ca2985433a87bf6f84330a79c93df0ddcd724a3
Kazen272 <cadudps@gmail.com>
2024-07-01 02:09:38 -0300

    Primeiro Commit

---
+++ carloslog/resources/views/layouts/main.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+
+        <!-- Css da aplicação -->
+        <link rel="stylesheet" href="/css/style.css">
+        
+        <!-- js da aplicação -->
+        <script src="/js/script.js"></script>
+        
+        <!-- bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+
+    </head>
+    <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="collapse navbar-collapse" id="navbar">
+                <a href="/" class="navbar-brand">
+                    <img src="/img/CarlosEduardo.png" alt="CarlosLog">
+                </a>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Home</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Entregas</a>
+                    </li>
+
+                    
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Entrar</a>
+                    </li>
+
+                    
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Cadastrar</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    @yield('content')    
+    <footer>
+        <p>CarlosLog &copy; 2024</p>
+    </footer> 
+    <!-- js da aplicação -->
+    <script src="/js/script.js"></script>  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>   
+    </body>
+</html>
---
+++ carloslog/resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('title', 'Carlos Log')
+
+@section('content')
+
+<h1>Olá mundo</h1>
+
+
+@endsection
